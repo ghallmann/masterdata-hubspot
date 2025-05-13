475b8c23b2ce71099b23a5259f83c7c3e68375f8
ghallmann <g.hallmann@madesa.com>
2025-05-13 14:35:33 -0300

    ref: Add withRetry and logs

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 import { getFirstBatch, getNextBatch } from "./vtexClient.js";
 import { upsertContacts } from "./hubspotClient.js";
+import { withRetry } from "./utils/utils.js";
+import { logger } from "./logger.js";
 
-const { token, data: firstBatch } = await getFirstBatch();
+const { token, data: firstBatch } = await withRetry(getFirstBatch, [], 5);
 
 let currentToken = token;
 let currentBatch = firstBatch;
 
 while (currentBatch.length > 0) {
   try {
-    const response = await upsertContacts(currentBatch);
-    if (response.status === 200) console.log("Batch upserted successfully");
+    const response = await withRetry(upsertContacts, [currentBatch], 5);
+    if (response.status === 200) logger.info("Batch upserted successfully");
   } catch (error) {
-    console.error("Error upserting contacts: ", error);
+    logger.error("Error upserting contacts: ", error);
   }
 
   try {
-    const nextBatch = await getNextBatch(currentToken);
+    const nextBatch = await withRetry(getNextBatch, [currentToken], 5);
     currentToken = nextBatch.token;
     currentBatch = nextBatch.data;
     if (currentBatch.length === 0) {
-      console.log("No more data to process");
+      logger.info("No more contacts to process");
       break;
     }
   } catch (error) {
-    console.error("Error requesting next contacts batch: ", error);
+    logger.error("Error requesting next contacts batch: ", error);
   }
 }
