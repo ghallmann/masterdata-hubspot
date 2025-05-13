9018135dc7a33fa455e09517468760fb50cc7185
ghallmann <g.hallmann@madesa.com>
2025-05-13 08:41:26 -0300

    fix: Correct endpoint and header

--- src/vtexClient.js
+++ src/vtexClient.js
@@ -3,27 +3,27 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const BASE_URL = process.env.VTEX_API_URL;
+const BASE_URL = process.env.VTEX_BASE_URL;
 const HEADERS = {
   "X-VTEX-API-AppKey": process.env.VTEX_APP_KEY,
   "X-VTEX-API-AppToken": process.env.VTEX_APP_TOKEN,
 };
 
 export async function getFirstBatch() {
-  const url = `${BASE_URL}/api/dataentities/LD/scroll?_size=100&_fields=email,lista,lastInteractionIn&_where=lastInteractionIn>2020-01-01`;
+  const url = `${BASE_URL}/scroll?_size=100&_fields=email,lista,lastInteractionIn&_where=lastInteractionIn>2020-01-01`;
   const response = await axios.get(url, { headers: HEADERS });
 
   return {
-    token: response.headers["X-VTEX-MD-TOKEN"],
+    token: response.headers["x-vtex-md-token"],
     data: response.data,
   };
 }
 
 export async function getNextBatch(token) {
-  const url = `${BASE_URL}/api/dataentities/LD/scroll?_token=${token}`;
+  const url = `${BASE_URL}/scroll?_token=${token}`;
   const response = await axios.get(url, { headers: HEADERS });
   return {
-    token: response.headers["X-VTEX-MD-TOKEN"],
+    token: response.headers["x-vtex-md-token"],
     data: response.data,
   };
 }
